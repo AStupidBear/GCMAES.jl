9555a672c698c3b16c53c0d8b7c4ade1ebb2149d
Yao Lu <luyaocns@gmail.com>
2018-07-06 22:03:36 +0800

    add call, remove throttle

--- src/util.jl
+++ src/util.jl
@@ -7,37 +7,7 @@ function ptp(x)
     xmax - xmin
 end
 
+call(f, xs...) = f(xs...)
+
 runall(f) = f
 runall(fs::AbstractVector) = () -> foreach(call, fs)
-
-function throttle(f, timeout; leading=true, trailing=false)
-  cooldown = true
-  later = nothing
-  result = nothing
-
-  function throttled(args...; kwargs...)
-    yield()
-
-    if cooldown
-      if leading
-        result = f(args...; kwargs...)
-      else
-        later = () -> f(args...; kwargs...)
-      end
-
-      cooldown = false
-      @schedule try
-        while (sleep(timeout); later != nothing)
-          later()
-          later = nothing
-        end
-      finally
-        cooldown = true
-      end
-    elseif trailing
-      later = () -> (result = f(args...; kwargs...))
-    end
-
-    return result
-  end
-end
