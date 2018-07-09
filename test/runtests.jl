4136e7ec873921ebd01481a610f42d7a986fcfb1
Yao Lu <luyaocns@gmail.com>
2018-07-09 14:34:43 +0800

    rastrigin

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,13 +7,13 @@ else
 end
 
 @everywhere begin
-    rastrigin(x) = -(10length(x) + sum(x.^2 .- 10 .* cos.(2π .* x)))
+    rastrigin(x) = 10length(x) + sum(x.^2 .- 10 .* cos.(2π .* x))
     ∇rastrigin(x) =  ForwardDiff.gradient(rastrigin, x)
     rastrigin′ = x -> (rastrigin(x), ∇rastrigin(x))
 end
 
 N = 2000; x0, σ0, lo, hi = 0.3ones(N), 0.2, fill(-5.12, N), fill(5.12, N)
-xmin, fmin, = GCMAES.maximize(rastrigin, x0, σ0, lo, hi; grad = false, maxiter = 3000)
-xmin, fmin, = GCMAES.maximize(rastrigin′, x0, σ0, lo, hi; grad = true, maxiter = 50, ν = 2)
+xmin, fmin, = GCMAES.minimize(rastrigin, x0, σ0, lo, hi; grad = false, maxiter = 3000)
+xmin, fmin, = GCMAES.minimize(rastrigin′, x0, σ0, lo, hi; grad = true, maxiter = 50, ν = 2)
 
 rm("CMAES.jld")
