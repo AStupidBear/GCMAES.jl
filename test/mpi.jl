a49f6e1e643e0824d1c867597ea5bf45697cf30b
Yao Lu <luyaocns@gmail.com>
2020-05-11 02:46:26 +0800

    Float32

--- test/mpi.jl
+++ test/mpi.jl
@@ -9,10 +9,10 @@ MPI.Initialized() || MPI.Init()
 rastrigin(x) = 10length(x) + sum(x.^2 .- 10 .* cos.(2π .* x))
 
 D = 5000
-x0 = fill(0.3, D)
-σ0 = 0.2
-lo = fill(-5.12, D)
-hi = fill(5.12, D)
+x0 = fill(0.3f0, D)
+σ0 = 0.2f0
+lo = fill(-5.12f0, D)
+hi = fill(5.12f0, D)
 
 GCMAES.minimize(rastrigin, x0, σ0, lo, hi, maxiter = 200)
 
