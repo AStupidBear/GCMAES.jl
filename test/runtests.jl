9d5ddf2dc9d83b07dc77d2786879b7eb7d677dd0
Yao Lu <luyaocns@gmail.com>
2020-06-07 20:39:02 +0800

    reduce test time

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,17 +5,17 @@ using Test
 
 @everywhere rastrigin(x) = 10length(x) + sum(x.^2 .- 10 .* cos.(2π .* x))
 
-D = 2000
+D = 200
 x0 = fill(0.3, D)
 σ0 = 0.2
 lo = fill(-5.12, D)
 hi = fill(5.12, D)
 
-GCMAES.minimize(rastrigin, x0, σ0, lo, hi, maxiter = 200, autodiff = false)
-GCMAES.minimize(rastrigin, x0, σ0, lo, hi, maxiter = 200, autodiff = true)
-GCMAES.minimize(rastrigin, x0, σ0, lo, hi, maxiter = 200, constr = BoxConstraint(lo, hi, 2))
-GCMAES.minimize(rastrigin, x0, σ0, lo, hi, maxiter = 200, constr = NormConstraint(2))
-GCMAES.minimize(rastrigin, x0, σ0, lo, hi, maxiter = 200, trans = BoxLinQuadTransform(lo, hi, scaling = false))
-GCMAES.minimize(rastrigin, x0, σ0, lo, hi, maxiter = 200, trans = BoxLinQuadTransform(lo, hi, scaling = true))
+GCMAES.minimize(rastrigin, x0, σ0, lo, hi, maxiter = 5, autodiff = false)
+GCMAES.minimize(rastrigin, x0, σ0, lo, hi, maxiter = 5, autodiff = true)
+GCMAES.minimize(rastrigin, x0, σ0, lo, hi, maxiter = 5, constr = BoxConstraint(lo, hi, 2))
+GCMAES.minimize(rastrigin, x0, σ0, lo, hi, maxiter = 5, constr = NormConstraint(2))
+GCMAES.minimize(rastrigin, x0, σ0, lo, hi, maxiter = 5, trans = BoxLinQuadTransform(lo, hi, scaling = false))
+GCMAES.minimize(rastrigin, x0, σ0, lo, hi, maxiter = 5, trans = BoxLinQuadTransform(lo, hi, scaling = true))
 
 rm("CMAES.bson", force = true)
