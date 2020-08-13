8188304342f7b9b68b190d5db09145d071546ee8
Yao Lu <luyaocns@gmail.com>
2020-08-13 13:33:56 +0800

    update

--- src/mpi.jl
+++ src/mpi.jl
@@ -1,39 +1,131 @@
-function part(x::AbstractArray{T, N}, dim = -1) where {T, N}
+worldcomm(comm = nothing) = something(comm, MPI.COMM_WORLD)
+
+worldsize(comm = nothing) = MPI.Initialized() ? MPI.Comm_size(worldcomm(comm)) : nworkers()
+
+myrank(comm = nothing) = MPI.Initialized() ? MPI.Comm_rank(worldcomm(comm)) : myid() - 1
+
+bcast(x, root = 0, comm = nothing) =  MPI.Initialized() ? MPI.bcast(x, root, worldcomm(comm)) : x
+
+allunique(x, comm = nothing) = sort(unique(allgather(unique(x), comm)))
+
+allequal(x, comm = nothing) = length(unique(allgather(x, comm))) == 1
+
+function allmean(x, comm = nothing)
+    if MPI.Initialized()
+        x = MPI.Allreduce(x, MPI.SUM, worldcomm(comm))
+        x = x / worldsize(comm)
+    end
+    return x
+end
+
+allmean(x::DateTime, comm = nothing) = unix2datetime(allmean(datetime2unix(x), comm))
+
+function allsum(x, comm = nothing)
+    if MPI.Initialized()
+        x = MPI.Allreduce(x, MPI.SUM, worldcomm(comm))
+    end
+    return x
+end
+
+function allmin(x, comm = nothing)
+    if MPI.Initialized()
+        x = MPI.Allreduce(x, MPI.SUM, worldcomm(comm))
+    end
+    return x
+end
+
+function allmax(x)
+    if MPI.Initialized()
+        x = MPI.Allreduce(x, MPI.MAX, MPI.COMM_WORLD)
+    end
+    return x
+end
+
+barrier(comm = nothing) = MPI.Initialized() ? MPI.Barrier(worldcomm(comm)) : nothing
+
+function part(x::AbstractArray{T, N}, comm = nothing; dims = -1) where {T, N}
     !MPI.Initialized() && return x
-    dim = clamp(dim > 0 ? dim : N + dim + 1, 1, N)
-    dsize, rank, wsize = size(x, dim), myrank(), worldsize()
+    dims = clamp(dims > 0 ? dims : N + dims + 1, 1, N)
+    dsize = size(x, dims) 
+    rank = myrank(comm)
+    wsize = worldsize(comm)
     if dsize >= wsize
         q, r = divrem(dsize, wsize)
         splits = cumsum([i <= r ? q + 1 : q for i in 1:wsize])
         pushfirst!(splits, 0)
         is = (splits[rank + 1] + 1):splits[rank + 2]
-        view(x, ntuple(x -> x == dim ? is : (:), N)...)
+        view(x, ntuple(x -> x == dims ? is : (:), N)...)
     else
         @warn "rank=$rank: dsize=$dsize < wsize=$wsize"
         is = (rank + 1):min(rank + 1, dsize)
-        view(x, ntuple(x -> x == dim ? is : (:), N)...)
+        view(x, ntuple(x -> x == dims ? is : (:), N)...)
     end
 end
 
-function allgather(x, dim = 1)
+function allgather(x, comm = nothing; dims = 1)
     if MPI.Initialized()
         x = isa(x, Number) ? [x] : x
-        counts = MPI.Allgather(Cint(length(x)), MPI.COMM_WORLD)
-        recvbuf = MPI.Allgatherv(vec(x), counts, MPI.COMM_WORLD)
+        counts = MPI.Allgather(Cint(length(x)), worldcomm(comm))
+        recvbuf = MPI.Allgatherv(vec(x), counts, worldcomm(comm))
         ranges = zip(cumsum([1; counts[1:end - 1]]), cumsum(counts))
-        shape = ntuple(i -> i == dim ? (:) : size(x, i), ndims(x))
+        shape = ntuple(i -> i == dims ? (:) : size(x, i), ndims(x))
         xs = [reshape(view(recvbuf, i:j), shape) for (i, j) in ranges]
         @assert sum(length, xs) == sum(counts)
-        return cat(xs..., dims = dim)
+        return cat(xs..., dims = dims)
     else
         return x
     end
 end
 
-myrank() = MPI.Initialized() ? MPI.Comm_rank(MPI.COMM_WORLD) : 0
+function gather(x, root, comm = nothing; dims = 1)
+    if MPI.Initialized()
+        x = isa(x, Number) ? [x] : x
+        counts = MPI.Allgather(Cint(length(x)), worldcomm(comm))
+        recvbuf = MPI.Gatherv(vec(x), counts, root, worldcomm(comm))
+        if myrank(comm) == root
+            ranges = zip(cumsum([1; counts[1:end - 1]]), cumsum(counts))
+            shape = ntuple(i -> i == dims ? (:) : size(x, i), ndims(x))
+            xs = [reshape(view(recvbuf, i:j), shape) for (i, j) in ranges]
+            @assert sum(length, xs) == sum(counts)
+            return cat(xs..., dims = dims)
+        else
+            return nothing
+        end
+    else
+        return x
+    end
+end
 
-bcast(x, root = 0) =  MPI.Initialized() ? MPI.bcast(x, root, MPI.COMM_WORLD) : x
+function localsize()
+    host = gethostname()
+    sendbuf = Vector{UInt8}(host * "\n")
+    recvbuf = allgather(sendbuf)
+    hosts = split(String(recvbuf))
+    count(hosts .== gethostname())
+end
 
-allequal(x) = length(unique(allgather(x))) == 1
+function pmap(f, xs)
+    if @isdefined(MPI) && MPI.Initialized()
+        comm, n = worldcomm(), length(xs)
+        wsize, rank = worldsize(), myrank()
+        if n >= wsize
+            allgather(map(f, part(xs)))
+        else
+            bsize = ceil(Int, wsize / n)
+            color = rank ÷ bsize
+            comm′ = MPI.Comm_split(comm, color, rank)
+            setlocalcomm!(comm′)
+            ys = allgather(f(xs[color + 1]))
+            setlocalcomm!(MPI.COMM_SELF)
+            ys[1:bsize:wsize]
+        end
+    else
+        Distributed.pmap(f, xs)
+    end
+end
 
-barrier() = MPI.Initialized() ? MPI.Barrier(MPI.COMM_WORLD) : nothing
+const _localcomm = Ref{MPI.Comm}(MPI.COMM_SELF)
+
+setlocalcomm!(comm) = _localcomm[] = comm
+
+localcomm() = _localcomm[]
