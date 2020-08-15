23828336178132f65c1898063bf47a9712f3a88b
Yao Lu <luyaocns@gmail.com>
2020-08-15 17:00:21 +0800

    update mpi test

--- test/mpi.jl
+++ test/mpi.jl
@@ -12,12 +12,12 @@ x0 = fill(0.3f0, D)
 lo = fill(-5.12f0, D)
 hi = fill(5.12f0, D)
 
-@mpirun GCMAES.minimize(x0, σ0, lo, hi, λ = 3, maxiter = 200) do x
-    GCMAES.allmean(rastrigin(x), GCMAES.localcomm())
-end
-
 @mpirun GCMAES.minimize(rastrigin, x0, σ0, lo, hi, λ = 200, maxiter = 200)
 
 @mpirun GCMAES.minimize(rastrigin, x0, σ0, lo, hi, λ = 200, maxiter = 200, autodiff = true)
+
+@mpirun GCMAES.minimize(x0, σ0, lo, hi, λ = 3, maxiter = 200) do x
+    GCMAES.allmean(rastrigin(x), GCMAES.localcomm())
+end
 
 rm("CMAES.bson", force = true)
